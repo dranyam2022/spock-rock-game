47b5847069f086cd307b5c9b8b352d619b0e42b2
dranyam2022 <maynardescalante@gmail.com>
2023-03-14 23:16:31 +0800

    corrected styling for mobile display

--- script.js
+++ script.js
@@ -18,12 +18,17 @@ const computerScore = document.querySelector(".computerScore");
 const reset = document.querySelector(".resetIcon");
 const winSound = document.querySelector(".win");
 const loseSound = document.querySelector(".lose");
+const playerContainer = document.querySelector(".player");
+const computerContainer = document.querySelector(".computer");
+const limitOptions = document.querySelectorAll(".limit-option");
 let pScore = 0;
 let cScore = 0;
+let scoreLimit = 3;
 
 /* playerPick function */
 function playerPick(evt) {
   const player = evt.target.getAttribute("title");
+
   winSound.load();
   loseSound.load();
   removeConfetti();
@@ -61,41 +66,76 @@ function checkWinner(player, computer) {
 function gameStart(evt) {
   const player = `${playerPick(evt)}`;
   const computer = `${computerPick()}`;
-
-  /* Player DOM manipulation */
-  const pSelected = evt.target;
-
-  playerOptions.forEach((option) => {
-    option.classList.remove("selected");
-  });
-
-  pSelected.classList.add("selected");
-  playerChoice.textContent = player.toUpperCase();
-
-  /* Computer DOM manipulation */
-  const cSelected = document.querySelector(`.computer${computer}`);
-
-  computerOptions.forEach((option) => {
-    option.classList.remove("selected");
-  });
-  cSelected.classList.add("selected");
-  computerChoice.textContent = computer.toUpperCase();
-
   /* Check Winner and update score*/
   const message = checkWinner(player, computer);
 
   resultText.textContent = message;
   playerScore.textContent = `${pScore}pts. `;
   computerScore.textContent = `${cScore}pts. `;
+
+  /* condition for maximum score */
+  if (pScore < scoreLimit && cScore < scoreLimit) {
+    /* Player DOM manipulation */
+    const pSelected = evt.target;
+
+    playerOptions.forEach((option) => {
+      option.classList.remove("selected");
+    });
+
+    pSelected.classList.add("selected");
+    playerChoice.textContent = player.toUpperCase();
+
+    /* Computer DOM manipulation */
+    const cSelected = document.querySelector(`.computer${computer}`);
+
+    computerOptions.forEach((option) => {
+      option.classList.remove("selected");
+    });
+    cSelected.classList.add("selected");
+    computerChoice.textContent = computer.toUpperCase();
+  } else {
+    if (pScore === scoreLimit) {
+      playerContainer.style.opacity = "0.3";
+      playerContainer.disabled = true;
+      playerContainer.style.pointerEvents = "none";
+      computerContainer.disabled = true;
+      computerContainer.style.opacity = "0.3";
+      resultText.textContent = "Congratulations! Reset to play again!";
+    } else {
+      playerContainer.style.opacity = "0.3";
+      playerContainer.disabled = true;
+      playerContainer.style.pointerEvents = "none";
+      computerContainer.disabled = true;
+      computerContainer.disabled = true;
+      computerContainer.style.opacity = "0.3";
+      resultText.textContent = "You Lost! Reset and try again!";
+    }
+  }
 }
+
 /* click event listener to playerOptions */
 playerOptions.forEach((option) => {
   option.addEventListener("click", gameStart);
 });
 
+/* score limit handler function */
+function scoreLimitHandler(event) {
+  limitOptions.forEach((option) => {
+    option.classList.remove("limit-option--selected");
+  });
+  scoreLimit = parseInt(event.target.nextElementSibling.value);
+  event.target.classList.add("limit-option--selected");
+}
+
+/* score limit option listener */
+limitOptions.forEach((option) => {
+  option.addEventListener("click", scoreLimitHandler);
+});
+
 /* resetHandler function */
 function resetHandler() {
-  location.reload();
+  pScore = 0;
+  cScore = 0;
 }
 /* reset button listener */
 reset.addEventListener("click", resetHandler);
